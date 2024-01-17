2bd25caa51578dd9c5ddb14d6aca7084fe424e7a
Ismael Ferreras <124872031+IsmaelFG@users.noreply.github.com>
2024-01-17 09:25:10 +0100

    Version 1.1

--- view/Layout.php
+++ view/Layout.php
@@ -13,27 +13,28 @@
         <title>Ismael Ferreras García</title>
         <link rel="stylesheet" href="webroot/css/bootstrap.css">
         <link rel="stylesheet" href="webroot/css/style.css">
+        <script src="webroot/js/bootstrap.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-expand-lg bg-primary">
             <div class="container">
                 <a class="navbar-brand text-white">LoginLogoff</a>
                 <?php require_once $view[$_SESSION['paginaActiva']]; ?>
-            <footer class="bg-primary text-light py-4 fixed-bottom">
-                <div class="container">
-                    <div class="row">
-                        <div class="col text-center text-white">
-                            <a href="/index.html">
-                                <p class="text-white">&copy; 2023/24 Ismael Ferreras García. Todos los derechos reservados.</p>
-                            </a>
-                        </div>
-                        <div class="col text-end">
-                            <a href="https://github.com/IsmaelFG/208DWESLoginLogoff" target="_blank">
-                                <img src="/webroot/imagenes/github-removebg-preview.png" alt="GitHub" width="35" height="35">
-                            </a>
+                <footer class="bg-primary text-light py-4 fixed-bottom">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col text-center text-white">
+                                <a href="/index.html">
+                                    <p class="text-white">&copy; 2023/24 Ismael Ferreras García. Todos los derechos reservados.</p>
+                                </a>
+                            </div>
+                            <div class="col text-end">
+                                <a href="https://github.com/IsmaelFG/208DWESLoginLogoff" target="_blank">
+                                    <img src="/webroot/imagenes/github-removebg-preview.png" alt="GitHub" width="35" height="35">
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </footer>
-    </body>
-</html>
+                </footer>
+                </body>
+                </html>
--- view/vInicioPublico.php
+++ view/vInicioPublico.php
@@ -14,11 +14,33 @@
     </button>
 </form>              
 <form method="post" action="">
-    <button type="submit" name="login" class="btn btn-primary">Login</button>
+    <button type="submit" name="login" class="btn btn-primary"><img src="webroot/imagenes/icono-user.png" alt="login"  width="30" height="30"></button>
 </form>
 <a class="navbar-brand text-white">Inicio Publico</a>
 </div>
 </nav>
+<div class="row mb-5">
+    <div class="col text-center">
+        <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="10000">
+                    <img src="webroot/imagenes/esquemaApp.PNG" class="img-fluid" alt="Mapeo de la Aplicación">
+                </div>
+                <div class="carousel-item" data-bs-interval="10000">
+                    <img src="webroot/imagenes/archivosApp.PNG" class="img-fluid" alt="Archivos de la Aplicación">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="false"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="false"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</div>
 <div class="position-absolute top-50 start-50 translate-middle">
     <h1>LoginLogoff</h1>
 </div>
